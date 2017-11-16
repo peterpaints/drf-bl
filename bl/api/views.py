36ededffa2afdd0307808f4017d527a019e9e196
peterpaints <musonyengigi@gmail.com>
2017-11-16 14:14:00 +0300

    [feature] Create the /bucketlists/ endpoint, with POST, GET, PUT and DELETE functions

--- bl/api/views.py
+++ bl/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
 
-# Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    """Create bucketlist endpoint."""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        """Save post data to db."""
+        serializer.save(created_by=self.request.user)
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """Handles GET, PUT, DELETE requests."""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
