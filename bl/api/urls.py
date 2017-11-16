21d64d7e956f6894c05f383123563839808bc09a
peterpaints <musonyengigi@gmail.com>
2017-11-17 01:14:22 +0300

    [chore] Add urls for extra bucketlists and items endpoints

--- bl/api/urls.py
+++ bl/api/urls.py
@@ -1,16 +1,19 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView, DetailsView, UserView, UserDetailsView
+from .views import BucketlistView, BucketlistDetailsView, ItemView, ItemDetailsView, UserView, CreateUserView, UserDetailsView
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = {
     url(r'^auth/', include('rest_framework.urls', namespace="rest_framework")),
-    url(r'^bucketlists/$', CreateView.as_view(), name="create"),
-    url(r'^bucketlists/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
+    url(r'^bucketlists/$', BucketlistView.as_view(), name="bucketlists"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/$', BucketlistDetailsView.as_view(), name="singlebucket"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/items/$', ItemView.as_view(), name="items"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/items/(?P<pk2>[0-9]+)/$', ItemDetailsView.as_view(), name="singleitem"),
     url(r'^users/$', UserView.as_view(), name="users"),
+    url(r'^users/register/$', CreateUserView.as_view(), name="register"),
     url(r'^users/(?P<pk>[0-9]+)/$', UserDetailsView.as_view(), name="user_details"),
-    url(r'^get-token/$', obtain_auth_token)
+    url(r'^login/$', obtain_auth_token)
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
