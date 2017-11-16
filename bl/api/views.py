3a7cc95e678e960ae7a10b7879747c678bac6de5
peterpaints <musonyengigi@gmail.com>
2017-11-17 01:15:07 +0300

    Have /bucketlists/ view return a list.
    In addition, implementing searching for GET /bucketlists/ with parameter 'q'.
    Also added were views for the /items/ actions and user registration.

--- bl/api/views.py
+++ bl/api/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
 from rest_framework import generics, permissions
-from .serializers import BucketlistSerializer, UserSerializer
-from .models import Bucketlist
+from .serializers import BucketlistSerializer, ItemSerializer, UserSerializer
+from .models import Bucketlist, Item
 from django.contrib.auth.models import User
 from .permissions import IsOwner
+from rest_framework import status
+from rest_framework.response import Response
 
 
-class CreateView(generics.ListCreateAPIView):
+class BucketlistView(generics.ListCreateAPIView):
     """Create bucketlist endpoint."""
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
@@ -16,12 +17,40 @@ class CreateView(generics.ListCreateAPIView):
         """Save post data to db."""
         serializer.save(created_by=self.request.user)
 
+    def get_queryset(self):
+        """Implement searching with parameter 'q'."""
+        queryset = Bucketlist.objects.all()
+        q = self.request.query_params.get('q', None)
+        if q is not None:
+            queryset = queryset.filter(name__icontains=q)
+        return queryset
 
-class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+
+class BucketlistDetailsView(generics.RetrieveUpdateDestroyAPIView):
     """Handles GET, PUT, DELETE requests."""
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
     permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+
+class ItemView(generics.ListCreateAPIView):
+    """Create item endpoint."""
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+    def perform_create(self, serializer):
+        """Save post data to db."""
+        serializer.save()
+
+
+class ItemDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """Handles GET, PUT, DELETE requests."""
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+    lookup_field = 'id'
+    lookup_url_kwarg = 'pk2'
 
 
 class UserView(generics.ListAPIView):
@@ -33,4 +62,11 @@ class UserView(generics.ListAPIView):
 class UserDetailsView(generics.RetrieveAPIView):
     """View to retrieve a user instance."""
     queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """View to register a user."""
+    model = User
+    permission_classes = [permissions.AllowAny]
     serializer_class = UserSerializer
