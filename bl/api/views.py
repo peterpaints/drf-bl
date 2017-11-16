d7836b501bd456bf98705bbeacc3867294585d81
peterpaints <musonyengigi@gmail.com>
2017-11-16 17:25:08 +0300

    [chore] Create endpoints for /users/ and /users/id

--- bl/api/views.py
+++ bl/api/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import BucketlistSerializer
+from rest_framework import generics, permissions
+from .serializers import BucketlistSerializer, UserSerializer
 from .models import Bucketlist
+from django.contrib.auth.models import User
+from .permissions import IsOwner
 
 
 class CreateView(generics.ListCreateAPIView):
     """Create bucketlist endpoint."""
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
 
     def perform_create(self, serializer):
         """Save post data to db."""
@@ -18,3 +21,16 @@ class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     """Handles GET, PUT, DELETE requests."""
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+
+class UserView(generics.ListAPIView):
+    """View to list the user queryset."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailsView(generics.RetrieveAPIView):
+    """View to retrieve a user instance."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
